0d3b166b6f0ec0ce33678bb252a9b511840b3527
Daniel Tabares <daniel.tabares1@udea.edu.co>
2023-11-21 17:38:47 -0500

    feat: Se arregla la función de actualizar rol

--- components/modales/usuarioModal.tsx
+++ components/modales/usuarioModal.tsx
@@ -1,4 +1,5 @@
 import Swal from "sweetalert2";
+import { API_ROUTES } from "@/service/apiConfig";
 
 interface UsuarioModalProps {
   name: string;
@@ -21,8 +22,29 @@ export const UsuarioModal = async ({ name, email }: UsuarioModalProps) => {
     confirmButtonText: "Editar Usuario",
   }).then((result) => {
     if (result.isConfirmed) {
+      const options = {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.4.2'},
+        body: JSON.stringify({email: email, roleId: result.value.toUpperCase()})
+      };  
+      fetch(API_ROUTES.users, options)
+      .then( response => {
+      })
+      .then( () => {
+        Swal.fire(
+          "Usuario actualizado correctamente",
+          "",
+          "success"
+        );
+      })
+      .catch( (error) => {
+        Swal.fire(
+          "500 No fue posible actualizar la información del usuario",
+          "",
+          "error"
+        );
+      });
       
-      Swal.fire("Se actualizo el ususario correctamente.", "", "success");
     } else if (result.isDenied) {
       Swal.fire(
         "No fue posible actualizar la información del usuario",
