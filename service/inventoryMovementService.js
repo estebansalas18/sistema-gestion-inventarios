62357c6a693da8a2266b52b822d753aa4379a754
Daniel Tabares <daniel.tabares1@udea.edu.co>
2023-11-25 00:34:17 -0500

    fix: get all inventory movements

--- service/inventoryMovementService.js
+++ service/inventoryMovementService.js
@@ -5,6 +5,24 @@ import { v4 as uuidv4 } from 'uuid';
 import Swal from "sweetalert2";
 
 const InventoryMovementService = {
+
+  getAllInventoryMovements: async () => {
+    try {
+      const { data, error } = await supabase
+        .from("InventoryMovement")
+        .select();
+
+      if (error) {
+        throw error;
+      }
+
+      return data;
+    } catch (error) {
+      console.error("Error al obtener movimientos de inventario en Supabase:", error);
+      throw error;
+    }
+  },
+
   createInventoryMovement: async (movementType, quantity, materialId, userId) => {
     try {
       // Obtener el saldo actual del material
